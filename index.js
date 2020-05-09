808576af5b6afe104505456333ec34411c6a6fde
phillmac <phillmackintosh@gmail.com>
2020-05-09 11:46:54 +0000

    Update node cmd

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Ipfs = require("ipfs");
-const OrbitDB = require("orbit-db");
-const { PeerManager, DBManager } = require("orbit-db-managers");
-const PeerStore = require("libp2p/src/peer-store");
-const PeerId = require("peer-id");
-const PeerInfo = require("peer-info");
-const multiaddr = require("multiaddr");
+const Ipfs = require('ipfs')
+const OrbitDB = require('orbit-db')
+const { PeerManager, DBManager } = require('orbit-db-managers')
+const PeerStore = require('libp2p/src/peer-store')
+const PeerId = require('peer-id')
+const PeerInfo = require('peer-info')
+const multiaddr = require('multiaddr')
 
-async function init() {
+async function init () {
   const ipfs = await Ipfs.create({
     preload: { enabled: true },
-    repo: "./ipfs",
+    repo: './ipfs',
     start: false,
     EXPERIMENTAL: { pubsub: true },
     libp2p: { config: { dht: { enabled: true } } },
@@ -19,32 +19,32 @@ async function init() {
         enabled: true // make this node a relay (HOP)
       }
     }
-  });
-  await ipfs.config.profiles.apply("server");
-  await ipfs.start();
-  await ipfs.ready;
+  })
+  await ipfs.config.profiles.apply('server')
+  await ipfs.start()
+  await ipfs.ready
 
-  const orbitdb = await OrbitDB.createInstance(ipfs);
+  const orbitdb = await OrbitDB.createInstance(ipfs)
   const peerMan = new PeerManager(ipfs, orbitdb, {
     PeerId,
     PeerInfo,
     multiaddr,
     PeerStore
-  });
-  const dbMan = new DBManager(orbitdb, peerMan, { logger: console });
+  })
+  const dbMan = new DBManager(orbitdb, peerMan, { logger: console })
 
-  const peers = await ipfs.swarm.peers();
-  console.dir({ peers });
-
-  for await (const p of ipfs.dht.findProvs(
-    "zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5"
-  )) {
-    console.dir({ p });
-  }
   const db = await dbMan.openCreate(
-    "/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test",
+    '/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test',
     { awaitLoad: false }
-  );
-  console.dir(dbMan.dbInfo(db));
+  )
+
+  const shutdown = async () => {
+    await orbitdb.close()
+    await ipfs.stop()
+    process.exit()
+  }
+
+  process.on('SIGINT', shutdown)
+  process.on('beforeExit', shutdown)
 }
-init();
+init()
