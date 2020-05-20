01eaf71437c2d99643550d65472ca13e9efdc634
phillmac <phillmackintosh@gmail.com>
2020-05-20 00:17:45 +0000

    Try simple ipfs addr first

--- connectPeers.js
+++ connectPeers.js
@@ -19,7 +19,9 @@ const connectPeers = function (options) {
         for (const prov of await peerMan.findPeers(db).search) {
           const provId = prov.id.toB58String()
           if (provId !== ipfsID && !(peers.some((p) => provId === p.peer))) {
-            const provAddrs = [] // prov.multiaddrs.toArray().map(a => `${a.toString()}/ipfs/${provId}`)
+            const provAddrs = [] //
+            // provAddrs.concat(prov.multiaddrs.toArray().map(a => `${a.toString()}/ipfs/${provId}`))
+            provAddrs.push(`/ipfs/${provId}`)
             provAddrs.push(`/p2p-circuit/ipfs/${provId}`)
             for (const a of provAddrs) {
               try {
