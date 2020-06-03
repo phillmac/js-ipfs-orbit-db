6220155f97839c1ea592e7be627e1cc6325eab87
phillmac <phillmackintosh@gmail.com>
2020-06-03 07:31:29 +0000

    Add api-ipfs.js

---
+++ api-ipfs.js
@@ -0,0 +1,10 @@
+const IpfsHttpClient= require('ipfs-http-client')
+
+async function run () {
+
+  const ipfs = await IpfsHttpClient('http://ipfs:5001')
+
+  const example = require('./example.js')(ipfs, () => {})
+  example()
+}
+run()
